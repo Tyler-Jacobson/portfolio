ffc573082ba1991a59e226b4f9e9c806bf33cf38
Tyler Jacobson <TylerJacobsonSE@gmail.com>
2020-12-19 18:40:38 -0800

    Foundation Complete

---
+++ src/Contact.js
@@ -0,0 +1,11 @@
+
+
+export default function Contact() {
+
+
+    return (
+        <div>
+            <h2>2</h2>
+        </div>
+    )
+}
---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export default function Home() {
+
+
+    return (
+        <div>
+            <h2>1</h2>
+        </div>
+    )
+}
